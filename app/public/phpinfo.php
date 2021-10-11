3fc1f393d5ba58d0665c774c936df5c1e911ca6c
Adithya Boyana <adithya.boyana@gmail.com>
2021-10-11 18:47:29 -0400

    Books data from database

---
+++ app/public/phpinfo.php
@@ -0,0 +1,35 @@
+<?php
+
+$num = 2;
+$foo = $num. " be";
+$bar = "or not".$num." be";
+
+echo $foo." ".$bar;
+
+echo &num * &num * &num;
+
+// this is a comment
+# this is also a comment
+
+/* this 
+is  a multi line comment */
+
+if (true) {
+    echo "True\n"
+}
+else 
+
+$arr = [1,1,2,3,4,5]
+
+$arr2 = [
+    "first" => "Tom",
+    "second" => "Bipin",
+    "best" => "DS"
+];
+
+$arr2 = [
+    "first" => "Tom",
+    "second" => "Bipin",
+    "best" => "DS"
+];
+echo json_encode ($arr2);
