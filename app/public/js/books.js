8c1b9379668dda124e0873b3cc0ffea035b73bb8
Adithya Boyana <adithya.boyana@gmail.com>
2021-10-20 21:05:09 -0400

    Insert Changes

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -3,55 +3,47 @@ const BooksData = {
     data() {
 
       return {
-
-        "books":[]
-
+        "books":[],
+        offerForm: {}
        }
-
     },
-
-   
 
     methods:{
-
-
-
       fetchBooksData() {
-
         fetch('/api/books/')
-
-        .then( response => response.json() )
-
+        .then( response => response.json())
         .then( (responseJson) => {
-
-            console.log(responseJson);
-
-            this.books = responseJson;
-
+          console.log(responseJson);
+          this.books = responseJson;
         })
-
         .catch( (err) => {
-
-            console.error(err);
-
+          console.error(err);
+      })         
+      },
+      postNewBook(evt) {
+        console.log("Posting!", this.offerForm);
+        fetch('api/books/create.php', {
+          method:'POST',
+          body: JSON.stringify(this.offerForm),
+          headers: {
+            "Content-Type": "application/json; charset=utf-8"
+          }
         })
-
+        .then( response => response.json() )
+        .then( json => {
+          console.log("Returned from post:", json);
+          // TODO: test a result was returned!
+          this.books = json;         
+          // reset the form
+          this.handleResetEdit();
+        });   
     }
-
     },
 
-
-
     created(){
-
      this.fetchBooksData();
-
+    //  this.postNewBook()
     }
 
-  }
-
-
-
- 
-
-  Vue.createApp(BooksData).mount('#BooksData1');
+}
+Vue.createApp(BooksData).mount('#BooksData1');
